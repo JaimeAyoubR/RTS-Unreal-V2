a38b2b6e370c231dba77fd57d9dbb113f77fcb82
JaimeAyoub <jaime.ayoub@saei.mx>
2025-06-04 11:32:58 -0600

    Clases que no se reflejan porque Unreal es una puta mierda

---
+++ Plugins/TopDown_Utilidades/Source/TopDown_Utilidades/Public/CommonEnumerator.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+UENUM(BlueprintType)
+enum class EResourceTypes: uint8
+{
+    Wood UMETA(DisplayName = "Wood"),
+    Food UMETA(DisplayName = "Food"),
+    Stone UMETA(DisplayName = "Stone"),
+    Water UMETA(DisplayName = "Water")
+    
+};
---
+++ Source/RTS/ResourceData.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ResourceData.h"
+
+ResourceData::ResourceData()
+{
+}
+
+ResourceData::~ResourceData()
+{
+}
